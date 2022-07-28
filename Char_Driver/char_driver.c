6d27c4f5fa149fd108e105c12ff7390569bb2dc6
Osamu-KJ <kevin.jerebica@gmail.com>
2022-07-28 16:29:25 -0400

    Fixed the class issue and refactored the code a bit

--- Char_Driver/char_driver.c
+++ Char_Driver/char_driver.c
@@ -3,32 +3,48 @@
 #include <linux/types.h>
 #include <linux/kdev_t.h>
 #include <linux/fs.h>
+#include <linux/device.h>
+#include <linux/cdev.h>
 
-dev_t dev; 
+const int major_number = 200; 
+const int minor_number = 0; 
+
+const char *module_name = "Chamu-r"; 
+const char *module_code_name = "chamur";
+
+dev_t dev = MKDEV(major_number, minor_number); 
 unsigned int dev_count = 10; 
+
+struct class *device_class; 
 
 static int __init hello_init(void) {
 
-    printk(KERN_ALERT "The module has been loaded\n"); 
+    printk("Loading the %s module\n", module_name); 
 
-    if (alloc_chrdev_region(&dev, 1, dev_count, "gang_shit")) {
-        printk(KERN_ALERT "BIG PROBLEMOO!!!\n");
+    if (register_chrdev_region(dev, dev_count, module_code_name)) {
+        printk(KERN_WARNING "[!!] Problem encountered while registring '%s' with the following numbers: (%d, %d) \n", module_code_name, major_number, minor_number);
         goto end; 
     }
 
-    printk(KERN_ALERT "Well that worked!!\n");
-    
+    device_class = class_create(THIS_MODULE, module_code_name);
+
+    printk(KERN_ALERT "[+] Successfully loaded '%s'\n", module_code_name);
+
     end:
         return 0; 
 }
 
 static void hello_exit(void) {
-    printk(KERN_ALERT "The module has been unloaded!\n");
+    printk("Unloading the %s module\n", module_name);
 
+    class_destroy(device_class); 
     unregister_chrdev_region(dev, dev_count);
+
+    printk(KERN_ALERT "[+] Successfully unloaded '%s'\n", module_code_name);
 }
 
 module_init(hello_init);
 module_exit(hello_exit);
 MODULE_AUTHOR("Osamu-KJ 2022"); 
 MODULE_DESCRIPTION("Simple hello world module");
+MODULE_LICENSE("GPL");
