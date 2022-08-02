5e993f00fc21cee6e0785452e7b920edddcb557f
Osamu-KJ <kevin.jerebica@gmail.com>
2022-08-02 17:21:28 -0400

    Fixed the code to create/destroy a device with sysfs when loading/unloading the module

--- Char_Driver/char_driver.c
+++ Char_Driver/char_driver.c
@@ -13,20 +13,63 @@ const char *module_name = "Chamu-r";
 const char *module_code_name = "chamur";
 
 dev_t dev = MKDEV(major_number, minor_number); 
-unsigned int dev_count = 10; 
-
+unsigned int dev_count = 2;
+struct file_operations fop; 
+struct cdev *cdevice;
+struct device *devicefs; 
 struct class *device_class; 
 
-static int __init hello_init(void) {
+
+static ssize_t chamur_read(struct file* file, char *buff, size_t len, loff_t *off) {
+    printk("AYO IM READING BRO~");
+    return 1; 
+}
+
+static ssize_t chamur_write(struct file* file, const char *buff, size_t len, loff_t *off) {
+    printk("AYO IM WRITING BRO~");
+    return 1; 
+}
+
+static int __init chamur_init(void) {
+
+    int ret; 
 
     printk("Loading the %s module\n", module_name); 
 
-    if (register_chrdev_region(dev, dev_count, module_code_name)) {
+    // setup the fop struct 
+    fop.owner = THIS_MODULE; 
+    fop.read = chamur_read; 
+    fop.write = chamur_write; 
+
+    ret = register_chrdev_region(dev, dev_count, module_code_name);
+
+    if (IS_ERR(ret)) {
         printk(KERN_WARNING "[!!] Problem encountered while registring '%s' with the following numbers: (%d, %d) \n", module_code_name, major_number, minor_number);
-        goto end; 
+        return ret;
     }
 
-    device_class = class_create(THIS_MODULE, module_code_name);
+    // allocate char device 
+    cdevice = cdev_alloc(); 
+
+    // initialize char device with the file operations structure 
+    cdev_init(cdevice, &fop);
+
+    // set the owner value 
+    cdevice->owner = THIS_MODULE; 
+
+    // add a char device to the system
+    ret = cdev_add(cdevice, dev, 1); 
+
+    if (IS_ERR(ret)) {
+        printk(KERN_WARNING "[!!] Problem encountered while initializing the cdev structure\n");
+        return ret; 
+    }
+
+    // make a class for the device 
+    device_class = class_create(THIS_MODULE, module_code_name); 
+
+    // register the device with sysfs 
+    devicefs = device_create(device_class, NULL, dev, NULL, module_code_name); 
 
     printk(KERN_ALERT "[+] Successfully loaded '%s'\n", module_code_name);
 
@@ -34,17 +77,26 @@ static int __init hello_init(void) {
         return 0; 
 }
 
-static void hello_exit(void) {
+static void chamur_exit(void) {
     printk("Unloading the %s module\n", module_name);
 
-    class_destroy(device_class); 
+    // remove the device from the sysfs 
+    device_destroy(device_class, dev); 
+
+    // destroy the class 
+    class_destroy(device_class);
+
+    // delete the cdev structure from the system 
+    cdev_del(cdevice); 
+
     unregister_chrdev_region(dev, dev_count);
 
-    printk(KERN_ALERT "[+] Successfully unloaded '%s'\n", module_code_name);
+    success: 
+        printk(KERN_ALERT "[+] Successfully unloaded '%s'\n", module_code_name);
 }
 
-module_init(hello_init);
-module_exit(hello_exit);
+module_init(chamur_init);
+module_exit(chamur_exit);
 MODULE_AUTHOR("Osamu-KJ 2022"); 
 MODULE_DESCRIPTION("Simple hello world module");
 MODULE_LICENSE("GPL");
